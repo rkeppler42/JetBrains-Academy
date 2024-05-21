01ad23c5468b3c657a96edaf4de968327df409a8
Roberto Keppler <167956475+rkeppler42@users.noreply.github.com>
2024-05-20 21:35:42 -0300

    Included Stage 4/4 of Bill Splitter

--- Introduction to Python/Challenging/Bill Splitter/billsplitter.py
+++ Introduction to Python/Challenging/Bill Splitter/billsplitter.py
@@ -27,13 +27,20 @@ def friends(n):
 def lucky_friend(friends_lst, bill):
     lucky_one = random.choice(friends_lst)
     print(f"{lucky_one} is the lucky one!")
+    pay_by_friend = {}
+    total_per_friend = round(bill / (len(friends_lst) - 1), 2)
+    for friend in friends_lst:
+        if friend != lucky_one:
+            pay_by_friend[friend] = total_per_friend
+        else:
+            pay_by_friend[friend] = 0
+    print(pay_by_friend)
 
 
 def no_lucky_friend(friends_lst, bill):
     print("No one is going to be lucky")
+    pay_by_friend = dict.fromkeys(friends_lst, round(bill / len(friends_lst), 2))
+    print(pay_by_friend)
 
 
 main()
-
-
-
