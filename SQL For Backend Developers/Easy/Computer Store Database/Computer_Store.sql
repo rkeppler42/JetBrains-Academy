39be3e5e5f88188b84271cac686629df4896ea58
Roberto Keppler <robertoreiskeppler@gmail.com>
2024-06-18 23:04:36 -0200

    Added the creator of the databased used on the project.

--- SQL For Backend Developers/Easy/Computer Store Database/Computer_Store.sql
+++ SQL For Backend Developers/Easy/Computer Store Database/Computer_Store.sql
@@ -42,19 +42,3 @@ CREATE TABLE Printer (
     PRIMARY KEY (code),
     FOREIGN KEY (model) REFERENCES Product(model)
 );
-
-/* Task 1: 
-
-Objectives:
-    - Identify printers in your inventory that are priced over $200;
-    - Find the model number, type, and price of the Printer. The order of the columns matters.*/
-
-SELECT 
-    model,
-    type,
-    price
-FROM
-    Printer
-WHERE
-    price > 200
-;
